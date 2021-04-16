1618152f91c4a226453262fbdda1a46b8fad006c
Hamed Jafari <hamedjix@gmail.com>
2021-04-17 02:43:15 +0430

    products UI

---
+++ front-end/src/screens/ProductScreen.jsx
@@ -0,0 +1,12 @@
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import Rating from '../components/Rating';
+import products from '../products';
+const ProductScreen = () => {
+  return (
+    <div>
+      <h1>Product</h1>
+    </div>
+  );
+};
+
+export default ProductScreen;
